2b99b48f109f80ec1fd13297be1d9b506b88dad6
ntorenduwayo <jpnduwntore@gmail.com>
2022-02-10 09:58:49 -0700

    Updating Data and Queries

---
+++ Queries/queries-create-tailored-list.sql
@@ -0,0 +1,161 @@
+ -- Delete retirement_info table:
+DROP TABLE retirement_info;
+-- Create new table for retiring employees i.e. retirement_info
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- Use Inner Join for Departments and dept-manager Tables:
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+-- Use Left Join to Capture retirement-info Table:
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+	retirement_info.last_name,
+    dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+-- Use Aliases for Code Readability:
+-- Joining retirement_info and dept_emp tables
+SELECT ri.emp_no,
+    ri.first_name,
+	ri.last_name,
+    de.to_date
+FROM retirement_info AS ri
+LEFT JOIN dept_emp AS de
+ON ri.emp_no = de.emp_no;
+
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments AS d
+INNER JOIN dept_manager AS dm
+ON d.dept_no = dm.dept_no;
+
+-- Use Left Join for retirement_info and dept_emp tables:
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+SELECT * FROM current_emp;
+
+--Use Count, Group By, and Order By:
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+-- create a new table (i.e. ret_emp_count), then export it as a CSV:
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO ret_emp_count
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+-- Create Additional Lists:
+-- List 1: Employee Information:
+-- According to our ERD, the Salaries table has a to_date column in it. Let's make sure it aligns with the employment date or something else.
+SELECT * FROM salaries
+-- We want to know what the most recent date on this list
+ORDER BY to_date DESC;
+
+-- Code to filter the Employees table to show only employes born and hired at the correct time,
+-- Now that our employees table has been filtered again and is being saved into a new temporary table (emp_info), 
+-- we need to join it to the salaries table to add the to_date and Salary columns to our query.
+SELECT e.emp_no,
+	e.first_name, 
+	e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info 
+FROM employees AS e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+
+-- List 2: Management
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+--INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+
+-- List 3: Department Retirees
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+-- INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+--SELECT * FROM retirement_info;
+-- Create a Tailored List:
+-- Create a query that will return only the information relevant to the Sales team.
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.dept_no,
+	d.dept_name
+FROM retirement_info AS ri
+INNER JOIN dept_emp AS de
+ON (ri.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_name = 'Sales';
+
+-- Create a query that will return the following information for the Sales and Development teams:
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.dept_no,
+	d.dept_name
+FROM retirement_info AS ri
+INNER JOIN dept_emp AS de
+ON (ri.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_name IN ('Sales', 'Development');
